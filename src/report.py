4812230fe31ee4f838ee8db0d8770db307903ab8
Ben Verley <benjamin.verley@computacenter.com>
2022-11-30 17:12:23 -0500

    incorperated failed drives for xtremIO and VMAX cleaned up formatting for csvs adjusted save location to be configurable updated config example

--- src/report.py
+++ src/report.py
@@ -11,15 +11,15 @@ import datetime
 CWD = os.path.dirname(__file__)
 CONFIGDIR=os.path.join(CWD,'config')
 CONFIGPATH=os.path.join(CONFIGDIR,'config.ini')
-CSVPATH = os.path.join(CWD,'CSVData')
 MODULESPATH = os.path.join(CWD,'modules')
-TB = ((1/1024)/1024)/1024
+GB = ((1/1024)/1024)
 time = datetime.datetime.now()
-DTIME = time.strftime("%Y_%m_%d_%H")
+DTIME = time.strftime("%Y%m%d%H")
 config = configparser.ConfigParser()
 config.read(CONFIGPATH)
 NOCOKEY = config.get('noco', 'api_key')
-NOCOURL = config.get('noco', 'url')
+NOCOURL = config.get('noco', 'urlbulk')
+CSVPATH = config.get('datapath', 'netpath')
 
 #Helper function to define dictionary after getting each data set from a device
 def makedict(device, used, failed, freespace, totalcap, rawpercent, percentage):
@@ -58,8 +58,7 @@ def makedict(device, used, failed, freespace, totalcap, rawpercent, percentage):
 
 #fucntion to write single device csvs
 def csvfunc(content, filename):
-    with open(filename, 'w') as file:
-        print("hello")
+    with open(filename, 'w', newline = '') as file:
         write = csv.DictWriter(file, content.keys())
         write.writeheader()
         write.writerow(content)
@@ -97,12 +96,20 @@ def main():
             rawpercent=1-(freespace/float(totcap))
             percentuse = rawpercent*100
 
+            #Get list of disks and then iterate over them to get failed disks
+            rdisks = requests.get(f'https://{ip}/api/json/v3/types/local-disks', auth=(usr, passw), headers=headers, verify =False)
+            count = 0
+            for disk in rdisks.json()['local-disks']:
+                r2 = requests.get(disk['href'], auth=(usr, passw), headers=headers, verify =False)
+                if r2.json()['content']['fru-lifecycle-state'] == 'failed':
+                    count = count +1
+
             #calls helper function to make dictionary and adds to running list
-            xtremiodict = makedict(device, round(used*TB, 3), 0, round(freespace*TB, 3), round(float(totcap)*TB, 3), rawpercent, percentuse)
+            xtremiodict = makedict(device, round(used*GB, 3), count, round(freespace*GB, 3), round(float(totcap)*GB, 3), rawpercent, percentuse)
             alldata.append(xtremiodict)
 
             #write singlecsv
-            csvfunc(xtremiodict['csvdict'], os.path.join(CSVPATH, f"{DTIME}_{device['Name']}.csv"))
+            csvfunc(xtremiodict['csvdict'], os.path.join(CSVPATH, f"{device['Name']}.csv"))
 
 
         #Pure Storage Devices
@@ -121,10 +128,10 @@ def main():
             totalspace = round(float(puredata['Capacity']), 3)
             
             #Calls helper function to make dict and adds to running list
-            puredict = makedict(device, usedspace, 0, freespace, totalspace, float(puredata['PercUsed']), float(puredata['Used(%)']))
+            puredict = makedict(device, usedspace*1024, 0, freespace*1024, totalspace*1024, float(puredata['PercUsed']), float(puredata['Used(%)']))
             alldata.append(puredict)
             #write singlecsv
-            csvfunc(puredict['csvdict'], os.path.join(CSVPATH, f"{DTIME}_{device['Name']}.csv"))
+            csvfunc(puredict['csvdict'], os.path.join(CSVPATH, f"{device['Name']}.csv"))
 
         #VMAX
         elif device['type']=="VMAX":
@@ -142,10 +149,24 @@ def main():
                 if split:
                     if split[0] == 'TBs':
                         percused=1-(float(split[2])/float(split[1]))
-                        vmaxdict = makedict(device, float(split[3]), 0, float(split[2]), float(split[1]), percused, percused*100)
-                        alldata.append(vmaxdict)
+                        vmaxdict = makedict(device, float(split[3])*1024, 0, float(split[2])*1024, float(split[1])*1024, percused, percused*100)
+                        
                         #write singlecsv
-                        csvfunc(vmaxdict['csvdict'], os.path.join(CSVPATH, f"{DTIME}_{device['Name']}.csv"))
+                        csvfunc(vmaxdict['csvdict'], os.path.join(CSVPATH, f"{device['Name']}.csv"))
+
+            #Get Failed drives
+            symdisk = subprocess.run(["symdisk", "-sid", f"{device['sid']}", "list", "-failed"], capture_output=True)
+            symdiskout = symcli.stdout.decode()
+            for line in io.StringIO(symdiskout):
+                if "Disks Selected" in line:
+                    split = line.split()
+                    driveno = split[-1]
+                else:
+                    driveno = "0"
+
+            vmaxdict['csvdict']['Failed'] = driveno
+            vmaxdict['nocodict']['Failed'] = driveno
+            alldata.append(vmaxdict)
 
         #Data Domain
         elif device['type']=="DataDomain":
@@ -163,9 +184,9 @@ def main():
             system = requests.get(f'https://{ip}:3009/rest/v1.0/system' , headers=headers, verify =False)
             
             #calc percentages
-            usedDD = (float(system.json()['physical_capacity']['used'])*TB)/1024
-            totalDD = (float(system.json()['physical_capacity']['total'])*TB)/1024
-            freespaceDD = (float(system.json()['physical_capacity']['available'])*TB)/1024
+            usedDD = (float(system.json()['physical_capacity']['used'])*GB)/1024
+            totalDD = (float(system.json()['physical_capacity']['total'])*GB)/1024
+            freespaceDD = (float(system.json()['physical_capacity']['available'])*GB)/1024
             rawpercentDD=round(1-(freespaceDD/totalDD), 4)
             percentuseDD = rawpercentDD*100
 
@@ -173,11 +194,11 @@ def main():
             alldata.append(DDdict)
 
             #write singlecsv
-            csvfunc(DDdict['csvdict'], os.path.join(CSVPATH, f"{DTIME}_{device['Name']}.csv"))
+            csvfunc(DDdict['csvdict'], os.path.join(CSVPATH, f"{device['Name']}.csv"))
 
     #Writes csv data based on data added to csv list
     nocodata=[]
-    with open(os.path.join(CSVPATH, f'{DTIME}_report.csv'), "w") as f:
+    with open(os.path.join(CSVPATH, f'report.csv'), "w", newline = '') as f:
         writer = csv.DictWriter(f, alldata[0]['csvdict'].keys())
         writer.writeheader()
         for row in alldata:
@@ -186,12 +207,12 @@ def main():
     
     #post data to noco in bulk
     header = {
-        'xc-auth' : NOCOKEY,
+        'xc-token' : NOCOKEY,
         'Content-Type' : 'application/json'
     }
 
-    response = requests.post(NOCOURL + 'testTable/bulk', headers=header, data = json.dumps(nocodata))
-
+    response = requests.post(NOCOURL + 'testTable', headers=header, data = json.dumps(nocodata))
+    print(response)
 
 
     
